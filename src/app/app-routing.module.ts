1166df93965c9442ba6b46ce9ce657b5d8b74e68
Devon Mason <devonmmason@gmail.com>
2019-11-26 18:50:21 -0500

    routing with films

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PeopleComponent } from './people/people.component'
+import { PeopleComponent } from './people/people.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { FilmsComponent } from './films/films.component';
 
 const routes: Routes = [
-  { path: 'people', component: PeopleComponent }
+  { path: 'people', component: PeopleComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'films', component: FilmsComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { People } from '../people';
 import { PeopleService } from '../people.service';
+import { Films } from '../films';
+import { FilmsService } from '../films.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,15 +11,22 @@ import { PeopleService } from '../people.service';
 })
 export class DashboardComponent implements OnInit {
   people: People[] = [];
+  films: Films[]= [];
 
-  constructor(private peopleService: PeopleService) { }
+  constructor(private peopleService: PeopleService, private filmsService: FilmsService) { }
 
   ngOnInit() {
     this.getPeople();
+    this.getFilms();
   }
 
   getPeople(): void {
     this.peopleService.getPeople()
       .subscribe(people => this.people = people.slice(1, 5));
+  }
+
+  getFilms(): void {
+    this.filmsService.getFilms()
+      .subscribe(films => this.films = films.slice(1,5));
   }
 }
